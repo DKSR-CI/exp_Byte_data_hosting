b13122da0859998c7bcc12ef03e95ff9f63f9ecb
LanduaDKSR <fabian.landua@dksr.city>
2024-05-16 12:21:44 +0200

    updated target dir

--- create_data_from_GUI.py
+++ create_data_from_GUI.py
@@ -10,28 +10,29 @@ current_dir = os.path.normpath(current_dir)
 current_dir = current_dir.replace("\\", "/")
 
 def download_data(data_export):
-    with open(current_dir + '/processed/chart_data.json', 'w') as fp:
+    with open(current_dir + '/data/gitprocessed/chart_data.json', 'w') as fp:
         json.dump(data_export, fp)
 
 def submit_data():
     city = entry.get()  # Get the data from the entry field
     try:
         export_1, LK_name = energy_data(city)
+        #print('load 1')
         export_2 = kfz_data(LK_name)
+        #print('load 2')
         export_3 = population_data(city)
+        #print('load 3')
         export_4 = mobile_data(city)
+        #print('load 4')
 
-        #print('Daten für ', city, '\n')
-        #print('Energiedaten:', export_1, '\n')
-        #print('KFZ-Bestand:', export_2, '\n')
-        #print('Bevölkerungsentwicklung:', export_3, '\n')
-        #print('Moblfunkabdeckung:', export_4, '\n')
         data_export = {'Energie':export_1, 'KFZ':export_2, 'Bevoelkerung':export_3, 'Mobilfunk':export_4}
-        label2 = tk.Label(root, text="Die neuen Daten wurden erfolgreich erstellt.", fg="green", height=5)
-        label2.pack()
 
         download_data(data_export)
-    except:
+        label2 = tk.Label(root, text="Die neuen Daten wurden erfolgreich erstellt.", fg="green", height=5)
+        label2.pack()
+    except Exception as error:
+        # handle the exception
+        print("An exception occurred:", error)
         label2 = tk.Label(root, text="Falsche Eingabe", fg="red", height=5)
         label2.pack()
 
