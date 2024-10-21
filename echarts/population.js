02785384c4abe2a99ca64264347535135f7f4670
LanduaDKSR <fabian.landua@dksr.city>
2024-10-21 18:01:38 +0200

    updated population data source

--- echarts/population.js
+++ echarts/population.js
@@ -12,8 +12,8 @@ var font_family = 'Inter Variable, Inter, sans-serif'
 
 var data = {
     'Jahr': [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023], 
-    'Gesamt': [128045, 129370, 131405, 132140, 133974, 135538, 137408, 136831, 137314, 138537, 141515], 
-    'Entwicklung': [1506, 1325, 2035, 735, 1834, 1564, 1870, -577, 483, 1223, 2978], 
+    'Gesamt': [129136, 131002, 132438, 133639, 135244, 136981, 137392, 136952, 138016, 141029, 142308], 
+    'Entwicklung': [1250, 1866, 1436, 1201, 1605, 1737, 411, -440, 1064, 3013, 1279], 
     'link': 'https://open.bydata.de/datasets/12411-000-d?locale=de'
 }
 
@@ -22,212 +22,220 @@ var svgContent = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xm
 
 
 function plus_minus(dataValue) {
-    if (dataValue >= 0) {
-        return '+' + dataValue;
-    } else {
-        return dataValue;
-    }
+  if (dataValue >= 0) {
+    return '+' + dataValue;
+  } else {
+    return dataValue;
+  }
 }
 
-if (Math.max(...data['Gesamt']) < 20000) {
-    var axis_max_trend = Math.ceil(Math.max(...data['Entwicklung'])/500)*1000
-    var axis_max_gesamt = Math.ceil(Math.max(...data['Gesamt'])/5000)*5000
+const gesamtMax = Math.max(...data['Gesamt']);
+const entwicklungMax = Math.max(...data['Entwicklung']);
+const entwicklungMin = Math.min(...data['Entwicklung']);
+const year_diff = Math.max(...data['Jahr']) - Math.min(...data['Jahr']);
+
+if (gesamtMax < 5000) {
+  var axis_max_trend = Math.ceil(entwicklungMax / 100) * 1000;
+  var axis_max_gesamt = Math.ceil(gesamtMax / 1000) * 1000;
+  var axis_min_trend = Math.floor(entwicklungMin / 100) * 100
+}
+else if (gesamtMax < 20000) {
+  var axis_max_trend = Math.ceil(entwicklungMax / 500) * 5000;
+  var axis_max_gesamt = Math.ceil(gesamtMax / 5000) * 5000;
+  var axis_min_trend = Math.floor(entwicklungMin / 1000) * 1000
 } else {
-    var axis_max_trend = Math.ceil(Math.max(...data['Entwicklung'])/5000)*10000
-    var axis_max_gesamt = Math.ceil(Math.max(...data['Gesamt'])/50000)*50000
+  var axis_max_trend = Math.ceil(entwicklungMax / 5000) * 50000;
+  var axis_max_gesamt = Math.ceil(gesamtMax / 50000) * 50000;
+  var axis_min_trend = Math.floor(entwicklungMin / 5000) * 5000
 }
 
-var axis_min_trend = Math.floor(Math.min(...data['Entwicklung'])/2000)*2000
-
-
-
-if (axis_min_trend < 0) {
-    var axis_min_gesamt = axis_min_trend * axis_max_gesamt / axis_max_trend;
-    var axis_interval = Math.abs(axis_min_gesamt);
-    var zero_label = 1;
-    var max_label = Math.floor(axis_max_gesamt/axis_interval) + 1
+if (entwicklungMin < 0) {
+  var axis_min_gesamt = axis_min_trend * axis_max_gesamt / axis_max_trend;
+  var axis_interval = Math.abs(axis_min_gesamt);
+  var zero_label = 1;
+  var max_label = Math.round(axis_max_gesamt / axis_interval) + 1;
 } else {
-    var axis_min_gesamt = 0;
-    var axis_interval = axis_max_gesamt/10
-    var zero_label = 0;
-    var max_label = 10
+  var axis_min_trend = 0;
+  var axis_min_gesamt = 0;
+  var axis_interval = axis_max_gesamt / 10;
+  var zero_label = 0;
+  var max_label = 10
 }
-
 
 var option = {
-    textStyle: { fontFamily: font_family },
-    title: [
-      {
-        text: data['Gesamt'].slice(-1).toLocaleString(),
-        subtext: 'Gesamt',
-        top: 30, //header_size * 2,
-        left: '3%',
-        textStyle: { color: colors[0], fontSize: header_size },
-        subtextStyle: { color: colors[0], fontSize: text_size }
-      },
-      {
-        text: plus_minus(data['Entwicklung'].slice(-1).toLocaleString()),
-        subtext: 'letztes Jahr',
-        top: 90, //header_size * 5,
-        left: '3%',
-        textStyle: { color: colors[1], fontSize: header_size },
-        subtextStyle: { color: colors[1], fontSize: text_size }
-      },
+  textStyle: { fontFamily: font_family },
+  title: [
+    {
+      text: data['Gesamt'].slice(-1).toLocaleString(),
+      subtext: 'Gesamt',
+      top: 30,
+      left: '3%',
+      textStyle: { color: colors[0], fontSize: header_size },
+      subtextStyle: { color: colors[0], fontSize: text_size }
+    },
+    {
+      text: plus_minus(data['Entwicklung'].slice(-1).toLocaleString()),
+      subtext: 'letztes Jahr',
+      top: 90,
+      left: '3%',
+      textStyle: { color: colors[1], fontSize: header_size },
+      subtextStyle: { color: colors[1], fontSize: text_size }
+    },
 
-    ],
-    grid: {
-      show: true,
-      backgroundColor: '#FAFAFB',
-      left: '20%',
-      width: '80%',
-      top: 5,
-      bottom: 0,
-      containLabel: true
-    },
-    tooltip: {
-      trigger: 'axis',
-      valueFormatter: (value) => value.toLocaleString(),
-      backgroundColor: '#FAFAFB',
-    },
-    xAxis: [
-      {
-        type: 'category',
-        boundaryGap: false,
-        axisLabel: {
-          formatter: function (value, index) {
-            if (index === 0 || index === 10) { // Display only first and last labels
-              return value;
-            } else {
-              return '';
-            }
+  ],
+  grid: {
+    show: true,
+    backgroundColor: '#FAFAFB',
+    left: '20%',
+    width: '80%',
+    top: 5,
+    bottom: 0,
+    containLabel: true
+  },
+  tooltip: {
+    trigger: 'axis',
+    valueFormatter: (value) => value.toLocaleString(),
+    backgroundColor: '#FAFAFB',
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      axisLabel: {
+        formatter: function (value, index) {
+          if (index === 0 || index === year_diff) { // Display only first and last labels
+            return value;
+          } else {
+            return '';
           }
-        },
-        data: data['Jahr']
-      }
-    ],
-    yAxis: [
-      {
-        type: 'value',
-        splitLine: {
-          show: false
-        },
-        axisLabel: {
-          formatter: function (value, index) {
-            if (index === zero_label || index === max_label) { // Display only first and last labels
-              return value.toLocaleString();
-            } else {
-              return '';
-            }
-          }
-        },
-        min: axis_min_gesamt,
-        max: axis_max_gesamt,
-        interval: axis_interval,
-        axisTick: {
-            inside: true
-        },
-      },
-      {
-        type: 'value',
-        show: false,
-        position: 'right',
-        min: axis_min_trend,
-        max: axis_max_trend,
-        splitLine: {
-          show: false
         }
-      }
-    ],
-    series: [
-      {
-        name: 'Gesamt',
-        type: 'line',
-        itemStyle: { color: colors[2] },
-        smooth: true,
-        showSymbol: false,
-        markPoint: {
-          animationDelay: 500,
-          data: [{ coord: [data['Jahr'].slice(-1).toString(), data['Gesamt'].slice(-1)], name: 'Last Symbol' }],
-          symbol: 'circle', // Set symbol shape
-          symbolSize: 6, // Set symbol size
-          label: {
-            show: false // Hide label except for the last symbol
-          },
-          itemStyle: {
-            color: colors[2] // Customize symbol color
-          }
-        },
-        label: {
-          show: true,
-          position: 'top'
-        },
-        lineStyle: {
-          color: colors[2],
-          width: 1
-        },
-        areaStyle: {
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: 'rgba(103, 197, 240, 1)'
-            },
-            {
-              offset: 1,
-              color: 'rgba(103, 197, 240, 0)'
-            }
-          ])
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: data['Gesamt']
       },
-      {
-        name: 'Entwicklung',
-        type: 'line',
-        itemStyle: { color: colors[3] },
-        smooth: true,
-        yAxisIndex: 1,
-        showSymbol: false,
-        markPoint: {
-          animationDelay: 500,
-          data: [{ coord: [data['Jahr'].slice(-1).toString(), data['Entwicklung'].slice(-1)], name: 'Last Symbol' }],
-          symbol: 'circle', // Set symbol shape
-          symbolSize: 6, // Set symbol size
-          label: {
-            show: false // Hide label for the last symbol
-          },
-          itemStyle: {
-            color: colors[3] // Customize symbol color
+      data: data['Jahr']
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      splitLine: {
+        show: false
+      },
+      axisLabel: {
+        formatter: function (value, index) {
+          if (index === zero_label || index === max_label) { // Display only first and last labels
+            return value.toLocaleString();
+          } else {
+            return '';
           }
-        },
-        label: {
-          show: true,
-          position: 'top'
-        },
-        lineStyle: {
-          color: colors[3],
-          width: 1
-        },
-        areaStyle: {
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: 'rgba(234, 124, 243, 1)'
-            },
-            {
-              offset: 1,
-              color: 'rgba(234, 124, 243, 0)'
-            }
-          ])
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: data['Entwicklung']//, 868]
+        }
+      },
+      min: axis_min_gesamt,
+      max: axis_max_gesamt,
+      interval: axis_interval,
+      axisTick: {
+        inside: true
+      },
+    },
+    {
+      type: 'value',
+      show: false,
+      position: 'right',
+      min: axis_min_trend,
+      max: axis_max_trend,
+      splitLine: {
+        show: false
       }
-    ]
-  };
+    }
+  ],
+  series: [
+    {
+      name: 'Gesamt',
+      type: 'line',
+      itemStyle: { color: colors[2] },
+      smooth: true,
+      showSymbol: false,
+      markPoint: {
+        animationDelay: 500,
+        data: [{ coord: [data['Jahr'].slice(-1).toString(), data['Gesamt'].slice(-1)], name: 'Last Symbol' }],
+        symbol: 'circle', // Set symbol shape
+        symbolSize: 6, // Set symbol size
+        label: {
+          show: false // Hide label except for the last symbol
+        },
+        itemStyle: {
+          color: colors[2] // Customize symbol color
+        }
+      },
+      label: {
+        show: true,
+        position: 'top'
+      },
+      lineStyle: {
+        color: colors[2],
+        width: 1
+      },
+      areaStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgba(103, 197, 240, 1)'
+          },
+          {
+            offset: 1,
+            color: 'rgba(103, 197, 240, 0)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: data['Gesamt']
+    },
+    {
+      name: 'Entwicklung',
+      type: 'line',
+      itemStyle: { color: colors[3] },
+      smooth: true,
+      yAxisIndex: 1,
+      showSymbol: false,
+      markPoint: {
+        animationDelay: 500,
+        data: [{ coord: [data['Jahr'].slice(-1).toString(), data['Entwicklung'].slice(-1)], name: 'Last Symbol' }],
+        symbol: 'circle', // Set symbol shape
+        symbolSize: 6, // Set symbol size
+        label: {
+          show: false // Hide label for the last symbol
+        },
+        itemStyle: {
+          color: colors[3] // Customize symbol color
+        }
+      },
+      label: {
+        show: true,
+        position: 'top'
+      },
+      lineStyle: {
+        color: colors[3],
+        width: 1
+      },
+      areaStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgba(234, 124, 243, 1)'
+          },
+          {
+            offset: 1,
+            color: 'rgba(234, 124, 243, 0)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: data['Entwicklung']//, 868]
+    }
+  ]
+};
 
 // Display the chart using the configuration items and data just specified.
 myChart.setOption(option);
